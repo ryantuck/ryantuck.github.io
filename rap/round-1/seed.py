8c4d94a703bd3354ab01a31ce37a5579c15eb94e
Ryan Tuck <ryan@ryantuck.io>
2025-06-12 11:13:32 -0400

    tidier

--- rap/round-1/seed.py
+++ rap/round-1/seed.py
@@ -1,11 +1,11 @@
 import glob
 
 
-def groups():
+def group_ids():
     return sorted(glob.glob('group-*'))
 
 
-def results():
+def rankings():
     return open('results.txt').read().splitlines()
 
 
@@ -13,23 +13,23 @@ def group(grp_id):
     return open(grp_id).read().splitlines()
 
 
-def group_results(group, rankings):
-    zipped = sorted(zip(rankings,group), key=lambda x: x[0])
-    return [s for r,s in zipped]
+def group_results(grp, grp_rankings):
+    zipped = sorted(zip(grp_rankings,grp), key=lambda x: x[0])
+    return [song for rank,song in zipped]
 
 
 def results_full():
     output = {}
-    for idx, grp in enumerate(groups()):
+    for idx, grp in enumerate(group_ids()):
         g = group(grp)
-        r = results()[idx]
+        r = rankings()[idx]
         gr = group_results(g, r)
         output[grp] = gr
     return output
 
 
-def print_pretty_group_results(gr):
-    for rank, song in enumerate(gr):
+def print_pretty_group_results(grp_ranked):
+    for rank, song in enumerate(grp_ranked):
         print(f'{rank+1}. {song}')
 
 
